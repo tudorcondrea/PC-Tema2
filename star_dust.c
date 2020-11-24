8a76878ec648a0a0289fa4ec2451c3342de5c85a
Condrea Tudor <tudorcondrea@yahoo.com>
2020-11-24 18:25:33 +0200

    Teoretic toate 3 implementate, practic inca apar erori

--- star_dust.c
+++ star_dust.c
@@ -16,7 +16,6 @@ char **read_img(int *n, int **v) //functie care returneaza un pointer la matrice
         for(j = 0; j < *(*v + i); j++)
         {
             scanf("%X", &temp);
-            //printf("%02X %02X %02X %02X\n", (temp&0xFF000000)>>24, (temp&0x00FF0000)>>16, (temp&0x0000FF00)>>8, temp &0x000000FF);
             img[i][j * 4 + 3] = (temp & 0xFF000000)>>24;
             img[i][j * 4 + 2] = (temp & 0x00FF0000)>>16;
             img[i][j * 4 + 1] = (temp & 0x0000FF00)>>8;
@@ -27,27 +26,31 @@ char **read_img(int *n, int **v) //functie care returneaza un pointer la matrice
     return img;
 }
 
-void modify(char **img, int *nr_col, int size, int line, int col, int val)
+void modify(char ***img, int *nr_col, int n, int size, int line, int col, int val)
 {
+    if(line > n)
+    {
+        *img = (char **)realloc(*img, line*sizeof(char **));
+    }
     if(col + 4 > nr_col[line])
     {
-        img[line] = (char *)realloc(img[line], (col + 4) * sizeof(char));
+        img[0][line] = (char *)realloc(img[line], (col + 4) * sizeof(char));
         nr_col[line] = col + 4;
     }
     if(size == 1){
-        img[line][col] = (char)val;
+        img[0][line][col] = (char)val;
     }
     else if(size == 2)
     {
-        img[line][col] = (char)(val & 0x000000FF);
-        img[line][col + 1] = (char)((val & 0x0000FF00)>>8);
+        img[0][line][col] = (char)(val & 0x000000FF);
+        img[0][line][col + 1] = (char)((val & 0x0000FF00)>>8);
     }
     else if(size == 4)
     {
-        img[line][col + 3] = (char)((val & 0xFF000000)>>24);
-        img[line][col + 2] = (char)((val & 0x00FF0000)>>16);
-        img[line][col + 1] = (char)((val & 0x0000FF00)>>8);
-        img[line][col] = (char)(val & 0x000000FF);
+        img[0][line][col + 3] = (char)((val & 0xFF000000)>>24);
+        img[0][line][col + 2] = (char)((val & 0x00FF0000)>>16);
+        img[0][line][col + 1] = (char)((val & 0x0000FF00)>>8);
+        img[0][line][col] = (char)(val & 0x000000FF);
     }
 }
 
@@ -77,7 +80,7 @@ void swap(char **img, int size, int line, int col)
     }
 }
 
-void modify_img(char **img, int *nr_col)
+void modify_img(char **img, int *nr_col, int n)
 {
     int k;
     char cmd, size;
@@ -97,7 +100,7 @@ void modify_img(char **img, int *nr_col)
         dist--;//datele de intrare dau blocurile incepand cu 1 iar coloanele mele incep de la 0
         dist *= (int)size;
         if(cmd == 'M')
-            modify(img, nr_col, (int)size, line, dist, val);
+            modify(&img, nr_col, n, (int)size, line, dist, val);
         if(cmd == 'D')
             delete(img, (int)size, line, dist);
         if(cmd == 'S')
@@ -127,15 +130,16 @@ double get_shield_power(int n, int *nr_col, char **img)
     return (double)sum_bytes/count_bytes;
 }
 
-int black_hole_finder(char **img, int *nr_col, int line, int col)
+int black_hole_finder(char **img, int *nr_col, int n, int line, int col)
 {
     char move_x[] = {1, 0, 0, -1};
     char move_y[] = {0, 1, -1, 0};
+    img[line][col] = -1;
     for(int i = 0; i < 4; i++)
-        if(line + (int)move_x[i] >= 0 && col + (int)move_y[i] < *(nr_col + line + (int)move_x[i]))
+        if(line + (int)move_x[i] >= 0 && line + (int)move_x[i] < n && col + (int)move_y[i] < *(nr_col + line + (int)move_x[i]) && col + (int)move_y[i] >= 0)
             if(img[line + (int)move_x[i]][col + (int)move_y[i]] == 0)
-                return 1 + black_hole_finder(img, nr_col, line + (int)move_x[i], col + (int)move_y[i]);
-    return 0;
+                return 1 + black_hole_finder(img, nr_col, n, line + (int)move_x[i], col + (int)move_y[i]);
+    return 1;
 }
 
 int main()
@@ -144,7 +148,7 @@ int main()
     int n, *nr_col;
     img = read_img(&n, &nr_col);
     printf("task 1\n%.8lf\n", get_shield_power(n, nr_col, img));
-    modify_img(img, nr_col);
+    modify_img(img, nr_col, n);
     printf("task 2\n");
     for (int i = 0; i < n; i++)
     {
@@ -157,12 +161,12 @@ int main()
         printf("\n");
     }
     int xmax = 0, ymax = 0, super_hole_size = 0, max_size = 0;
-    /*for(int i = 0; i < n; i++)
+    for(int i = 0; i < n; i++)
     {
         for(int j = 0; j < *(nr_col + i); j++)
             if(img[i][j] == 0)
             {
-                super_hole_size = black_hole_finder(img, nr_col, i, j);
+                super_hole_size = black_hole_finder(img, nr_col, n, i, j);
                 if(super_hole_size > max_size)
                 {
                     max_size = super_hole_size;
@@ -170,8 +174,7 @@ int main()
                     ymax = j;
                 }
             }
-    }*/
-    max_size = black_hole_finder(img, nr_col, 9, 8);
+    }
     printf("task 3\n%d %d %d\n", xmax, ymax, max_size);
     for(int i = 0; i < n; i++)
         free(*(img + i));
